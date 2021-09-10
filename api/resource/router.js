6dc08cbdaac17e6172442466f849a973a14b94e5
ashleyannlaz <paperpastels@gmail.com>
2021-09-10 09:53:50 -0400

    Resource Endpoints Built

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
-// build your `/api/resources` router here
+const express = require('express');
+const resources = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    resources.getResources()
+    .then(resources => {
+        res.status(200).json(resources)
+    })
+    .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+    resources.createResource(req.body)
+    .then(resource => {
+        res.status(201).json(resource)
+    })
+    .catch(next)
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const projectRouter = require('./project/router')
+const taskRouter = require('./task/router')
+const resourceRouter = require('./resource/router')
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/resources', resourceRouter)
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+
+  module.exports = server;
